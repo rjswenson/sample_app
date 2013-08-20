9736acaf07b218058c8c89306893f9e49fa971ce
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-20 09:18:16 -0700

    fixed breaking tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,7 +20,20 @@ describe "Static pages" do
   end
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+  it "should have the right links within layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact Us'))
+
+    visit root_path
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(""))
   end
 end
