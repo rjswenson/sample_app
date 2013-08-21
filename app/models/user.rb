40add71b4fe6aa31e7d4d5c9609fd53ce2b8e685
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-21 08:14:15 -0700

    adds password validation via has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }    #uniqueness defaults true if passed an arg
-
-  #has_secure_password  #encrypts and checks password for confirmations
+  has_secure_password  #encrypts and checks password for confirmations
+  validates :password, length: { minimum: 6 }
 end
