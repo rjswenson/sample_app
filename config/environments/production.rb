189640b2091bab9da9d09cc0b1b5970c35d2e95c
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-22 09:52:23 -0700

    adds SSL in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,10 @@ SampleApp::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
