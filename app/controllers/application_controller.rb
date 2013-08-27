2667a37321186c2c27bac5120e0598e7cef2bcca
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-27 10:55:20 -0700

    typo SessionHelper to SessionsHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include SessionHelper  #includes SessionHelper module in all controllers
+  include SessionsHelper  #includes SessionHelper module in all controllers
 end
 
 
