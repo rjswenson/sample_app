a40943ce7bd6f5d89ab74b3279e86ab230ff68dd
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-27 09:28:06 -0700

    fixes typo from first to sample app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-FirstApp::Application.config.session_store :cookie_store, key: '_first_app_session'
+SampleApp::Application.config.session_store :cookie_store, key: '_first_app_session'
