da7ba3ad3b0bb6d829dc30cb308f58636e86b2d1
Robin Swenson-Healey <rjswenson@gmail.com>
2013-09-19 14:07:15 -0700

    adds edit and update to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
   def create
     @user = User.new(user_params)
     
@@ -15,6 +16,19 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #handle a successful update
+    else
+      render 'edit'
     end
   end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper'
 describe "UserPages" do
   subject {page}
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "the page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+  end
+  
   describe "signup page" do
     before {visit signup_path}
 
