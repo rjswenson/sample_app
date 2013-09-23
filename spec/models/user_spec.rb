b8797538e2f30d46d661730f8c4f3b3ec56f7acd
Robin Swenson-Healey <rjswenson@gmail.com>
2013-09-23 11:29:33 -0700

    user db with admin boolean

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,19 @@ describe User do
   it {should respond_to(:password_confirmation)}
   it {should respond_to(:remember_token)}  #token to keep track of persistant login
   it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
 
   it {should be_valid}
+  it {should_not be_admin}
+
+  describe " with admin attributes set to TRUE" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it {should be_admin}
+  end
 
   describe "when name is not present" do
     before { @user.name = " "}
