07989b77b0520ee71de570828f47e7f9109fea69
Robin Swenson-Healey <rjswenson@gmail.com>
2013-09-30 10:59:52 -0700

    paginates microposts on user home page when logged

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
@@ -18,5 +19,10 @@ class MicropostsController < ApplicationController
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,16 @@ describe User do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+
     it "should be newest to oldest order" do
       expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
     end
