71a5de42d5b53cd13ab75b2911a50609824af850
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-04 12:52:36 -0700

    follow/unfollow buttons call create/destroy in Rel Controller

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -54,6 +54,18 @@ describe "Authentication" do
       describe "for non-signed-in users" do
         let(:user) { FactoryGirl.create(:user) }
         
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end
+        end
+
         describe "in Microposts controller" do
           describe "submitting a create action" do
             before { post microposts_path }
